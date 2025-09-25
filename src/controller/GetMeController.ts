b2d218e3573da7a3a509bce18fcfbd50a872ab38
mateusbrbza <mateus.c.barboza@gmail.com>
2025-09-25 20:20:47 -0300

    finished getmecontroller

--- src/controller/GetMeController.ts
+++ src/controller/GetMeController.ts
@@ -1,8 +1,24 @@
+import { database } from '~/db';
 import { HttpResponse, ProtectedHttpRequest } from '../types/Http';
 import { ok } from '../utils/http';
+import { eq } from 'drizzle-orm';
+import { usersTable } from '~/db/schema';
 
 export class GetMeController {
   static async handle({ userId }: ProtectedHttpRequest): Promise<HttpResponse> {
-    return ok({ userId })
+    const user = await database.query.usersTable.findFirst({
+      columns: {
+        id: true,
+        email: true,
+        name: true,
+        calories: true,
+        proteins: true,
+        carbohydrates: true,
+        fats: true,
+      },
+      where: eq(usersTable.id, userId),
+    })
+
+    return ok({ user })
   }
 }
