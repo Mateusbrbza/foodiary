de1fef19a75d289c1821a1174e28980eedd79934
mateusbrbza <mateus.c.barboza@gmail.com>
2025-09-25 10:50:32 -0300

    error handling on me handler

--- src/functions/me.ts
+++ src/functions/me.ts
@@ -1,11 +1,18 @@
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 
 import { GetMeController } from "~/controller/GetMeController";
+import { unauthorized } from "~/utils/http";
 import { parseProtectedEvent } from "~/utils/parseProtectedEvent";
 import { parseResponse } from "~/utils/parseResponse";
 
 export async function handler(event: APIGatewayProxyEventV2) {
-  const request = parseProtectedEvent(event);
-  const response = await GetMeController.handle(request);
-  return parseResponse(response);
+  try {
+    const request = parseProtectedEvent(event);
+    const response = await GetMeController.handle(request);
+    return parseResponse(response);
+  } catch {
+    return parseResponse(
+      unauthorized({ error: 'Invalid Access Token.' })
+    )
+  }
 }
