c761e8dcddb80f0c2e49bed2b577d580dce75bb5
mateusbrbza <mateus.c.barboza@gmail.com>
2025-10-01 13:10:39 -0300

    get meal by id route working. / chore: start working on s3 for image upload

---
+++ src/controller/GetMealByIdController.ts
@@ -0,0 +1,37 @@
+import z from 'zod';
+import { database } from '~/db';
+import { HttpResponse, ProtectedHttpRequest } from '../types/Http';
+import { badRequest, ok } from '../utils/http';
+import { and, eq } from 'drizzle-orm';
+import { mealsTable } from '~/db/schema';
+
+const schema = z.object({
+  mealId: z.uuid(),
+})
+
+export class GetMealByIdController {
+  static async handle({ userId, params }: ProtectedHttpRequest): Promise<HttpResponse> {
+    const { success, error, data } = schema.safeParse(params)
+
+    if (!success) {
+      return badRequest({ errors: error.issues })
+    }
+
+    const meal = await database.query.mealsTable.findFirst({
+      columns: {
+        id: true,
+        foods: true,
+        createdAt: true,
+        icon: true,
+        name: true,
+        status: true,
+      },
+      where: and(
+        eq(mealsTable.id, data.mealId),
+        eq(mealsTable.userId, userId),
+      )
+    });
+
+    return ok({ meal });
+  }
+}
